ec415ed712aad7be4c19bd0122be089a6ec248ca
Ramireddy Mallikarjuna Reddy <rrmr3119@gmail.com>
2024-07-25 23:13:31 +0530

    Time: 34 ms (65.16%) | Memory: 16.5 MB (17.54%) - LeetSync

---
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minBitFlips(self, start: int, goal: int) -> int:
+        ans = start ^ goal
+        count = 0
+        for i in range(31):
+            if ans & (1<<i):
+                count += 1
+        return count
+        
