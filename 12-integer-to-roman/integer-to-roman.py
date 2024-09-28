545b40ab8255bb715084040193c2628b9354a20f
Ramireddy Mallikarjuna Reddy <rrmr3119@gmail.com>
2024-09-28 15:01:03 +0530

    Time: 56 ms (14.54%) | Memory: 16.5 MB (59.50%) - LeetSync

---
+++ 12-integer-to-roman/integer-to-roman.py
@@ -0,0 +1,14 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        mpp = {
+            1000:"M", 900:"CM", 500:"D", 400 :"CD", 100:"C", 90:"XC",
+            50:"L", 40:"XL", 10 :"X", 9: "IX", 5:"V",4:"IV",1:"I"
+        }
+
+        res = []
+        for value in mpp.keys():
+            while num >= value:
+                res.append(mpp[value])
+                num -= value
+        ans = ''.join(res)
+        return ans
