d5ccd424ebcc42d93e59c0b56bf1a21036209773
Ramireddy Mallikarjuna Reddy <rrmr3119@gmail.com>
2024-10-07 23:35:27 +0530

    Time: 39 ms (81.05%) | Memory: 16.7 MB (7.81%) - LeetSync

--- 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.py
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.py
@@ -1,11 +1,15 @@
 class Solution:
     def minLength(self, s: str) -> int:
-        st = []
-        for char in s:
-            st.append(char)
-            if len(st) >= 2 and ((st[-1] == "B" and st[-2]=="A") or (st[-1] == "D" and st[-2]=="C")):
-                st.pop()
-                st.pop()
-        return len(st)
-                
-        
+        while True:
+            f = True
+            if 'AB' in s:
+                s = s.replace('AB', '')
+                f = False
+            
+            if 'CD' in s:
+                s = s.replace('CD', '')
+                f = False
+
+            if f:
+                return len(s)
+            
