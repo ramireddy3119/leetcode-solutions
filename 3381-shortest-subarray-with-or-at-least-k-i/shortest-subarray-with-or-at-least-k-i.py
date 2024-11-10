14bff2a93f99d7da82bff3030939c668a29174d7
Ramireddy Mallikarjuna Reddy <rrmr3119@gmail.com>
2024-11-10 14:22:57 +0530

    Time: 26 ms (12.90%) | Memory: 16.7 MB (13.39%) - LeetSync

--- 3381-shortest-subarray-with-or-at-least-k-i/shortest-subarray-with-or-at-least-k-i.py
+++ 3381-shortest-subarray-with-or-at-least-k-i/shortest-subarray-with-or-at-least-k-i.py
@@ -1,15 +1,33 @@
-from typing import List
-
 class Solution:
-    def minimumSubarrayLength(self, nums: List[int], k: int) -> int:
-        length = float('inf') 
+    def minimumSubarrayLength(self, nums: list[int], k: int) -> int:
+        if k == 0:
+            return 1
+        
+        precomp = [0] * 32
+        st = 0
+        ans = float('inf')
         
         for i in range(len(nums)):
-            xor,ans = 0,0
-            for j in range(i, len(nums)):
-                xor |= nums[j]  
-                ans  += 1
-                if xor >= k:
-                    length = min(length,ans)
+            val = 0
+            for j in range(32):
+                if nums[i] & (1 << j):
+                    precomp[j] += 1
+                if precomp[j]:
+                    val |= (1 << j)
+            
+            if val >= k:
+                ans = min(ans, i - st + 1)
+            
+            while val >= k:
+                val = 0
+                for j in range(32):
+                    if nums[st] & (1 << j):
+                        precomp[j] -= 1
+                    if precomp[j]:
+                        val |= (1 << j)
+                st += 1
+                
+                if val >= k:
+                    ans = min(ans, i - st + 1)
         
-        return length if length != float('inf') else -1
+        return ans if ans != float('inf') else -1
