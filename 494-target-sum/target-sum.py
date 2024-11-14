4a72014bc676b459cdf09d4adb3452e0f8aa03b1
Ramireddy Mallikarjuna Reddy <rrmr3119@gmail.com>
2024-11-15 00:04:52 +0530

    Time: 39 ms (89.49%) | Memory: 17 MB (67.47%) - LeetSync

--- 494-target-sum/target-sum.py
+++ 494-target-sum/target-sum.py
@@ -2,28 +2,24 @@ from typing import List
 
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        def countPartitionsUtil(ind,tar,arr,dp):
-            if ind  == 0:
-                if tar == 0 and arr[0] == 0: return 2
-                if tar == 0 or tar == arr[0]: return 1
-                return 0
-            if dp[ind][tar] != -1:
-                return dp[ind][tar]
-            
-            notTake = countPartitionsUtil(ind-1,tar,arr,dp)
-            take = 0
-            if arr[ind] <= tar:
-                take = countPartitionsUtil(ind-1,tar-arr[ind],arr,dp)
-            dp[ind][tar] = (take+notTake)
-            return dp[ind][tar]
-        def findWays(arr,d):
-            n = len(arr)
-            totSum = sum(arr)
-            
-            if totSum - d < 0: return 0
-            if (totSum - d) % 2 == 1: return 0
-            
-            s2 = (totSum-d)//2
-            dp  = [[-1 for j in range(s2+1)]for i in range(n)]
-            return countPartitionsUtil(n-1,s2,arr,dp)
-        return findWays(nums,target)
+        def findWays(nums,tar):
+            n = len(nums)
+            dp = [[0]*(tar+1) for _ in range(n)]
+            if nums[0] == 0:
+                dp[0][0] = 2
+            else:
+                dp[0][0] = 1
+            if nums[0] <= tar and nums[0] != 0:
+                dp[0][nums[0]] = 1
+            for ind in range(1,n):
+                for target in range(tar+1):
+                    notTaken = dp[ind-1][target]
+                    take = 0
+                    if nums[ind] <= target:
+                        take = dp[ind-1][target-nums[ind]]
+                    dp[ind][target] = take + notTaken
+            return dp[n-1][tar]
+        totSum = sum(nums)
+        if totSum - target < 0: return 0
+        if (totSum - target) % 2 == 1: return 0
+        return findWays(nums,(totSum-target)//2)
