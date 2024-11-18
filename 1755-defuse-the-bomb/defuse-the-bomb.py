7fd8ab5a306c8790701d575bf0eef7e0a2eb2afe
Ramireddy Mallikarjuna Reddy <rrmr3119@gmail.com>
2024-11-18 19:34:09 +0530

    Time: 0 ms (100.00%) | Memory: 16.7 MB (22.92%) - LeetSync

--- 1755-defuse-the-bomb/defuse-the-bomb.py
+++ 1755-defuse-the-bomb/defuse-the-bomb.py
@@ -1,20 +1,33 @@
 class Solution:
     def decrypt(self, code: List[int], k: int) -> List[int]:
+        n = len(code)
         if k == 0:
             return [0]*len(code)
+        # res = []
+        # if k > 0:
+        #     for i in range(n):
+        #         curr_sum = 0
+        #         for j in range(i+1,i+k+1):
+        #             curr_sum += code[j%n]
+        #         res.append(curr_sum)
+        # else:
+        #     for i in range(n):
+        #         curr_sum = 0
+        #         for j in range(i-1, i+k-1, -1):
+        #             curr_sum += code[j % n]
+        #         res.append(curr_sum)
+        code += code
+        prefix_sum = []
+        curr_sum = 0
+        for i in range(len(code)):
+            curr_sum += code[i]
+            prefix_sum.append(curr_sum)
         res = []
-        n = len(code)
-        if k > 0:
+        if k >0:
             for i in range(n):
-                curr_sum = 0
-                for j in range(i+1,i+k+1):
-                    curr_sum += code[j%n]
-                res.append(curr_sum)
+                res.append(prefix_sum[i+k]-prefix_sum[i])
         else:
-            for i in range(n):
-                curr_sum = 0
-                for j in range(i-1, i+k-1, -1):
-                    curr_sum += code[j % n]
-                res.append(curr_sum)
-
+            k = abs(k)
+            for i in range(n,2*n):
+                res.append(prefix_sum[i-1]-prefix_sum[i-k-1])    
         return res
