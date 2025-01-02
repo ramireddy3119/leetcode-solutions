1f326af5b5821fe78e3dbf726a683ce8efa1607e
Ramireddy Mallikarjuna Reddy <rrmr3119@gmail.com>
2025-01-02 19:32:16 +0530

    Time: 23 ms (46.02%) | Memory: 49.7 MB (47.74%) - LeetSync

--- 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.py
+++ 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.py
@@ -1,18 +1,31 @@
-from typing import List
-
 class Solution:
     def vowelStrings(self, words: List[str], queries: List[List[int]]) -> List[int]:
-        def is_vowel_word(word):
-            return word[0].lower() in "aeiou" and word[-1].lower() in "aeiou"
-        
-        n = len(words)
-        prefix_sum = [0] * (n + 1)
-        
-        for i in range(n):
-            prefix_sum[i + 1] = prefix_sum[i] + (1 if is_vowel_word(words[i]) else 0)
-        
         res = []
-        for l, r in queries:
-            res.append(prefix_sum[r + 1] - prefix_sum[l])
-        
+        # for i in queries:
+        #     count = 0
+        #     for j in range(i[0],i[-1]+1):
+        #         word = words[j]
+        #         if word[0].lower() in "aeiou" and word[-1].lower() in "aeiou":
+        #             count += 1
+        #     res.append(count)
+        prefix_sum = [0]*len(words)
+        word = words[0]
+        if word[0].lower() in "aeiou" and word[-1].lower() in "aeiou":
+            prefix_sum[0] = 1
+        else:
+            prefix_sum[0] = 0
+        for i in range(1,len(words)):
+            wrd = words[i]
+            if wrd[0].lower() in "aeiou" and wrd[-1].lower() in "aeiou":
+                prefix_sum[i] = prefix_sum[i-1]+1
+            else:
+                prefix_sum[i] = prefix_sum[i-1]
+        for l,r in queries:
+            if l == 0:
+                res.append(prefix_sum[r])
+            else:
+                res.append(prefix_sum[r]-prefix_sum[l-1])
         return res
+
+            
+
