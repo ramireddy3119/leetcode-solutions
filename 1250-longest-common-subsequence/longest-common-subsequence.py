80fd6cf8da1500423d7ade780393ef6a6d1b12ef
Ramireddy Mallikarjuna Reddy <rrmr3119@gmail.com>
2025-01-02 22:39:19 +0530

    Time: 464 ms (60.38%) | Memory: 42.8 MB (40.60%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -3,14 +3,15 @@ class Solution:
         m = len(text1)
         n = len(text2)
         dp = [[-1]*(n+1) for _ in range(m+1)]
-        def longsub(ind1,ind2):
-            if ind1 < 0 or ind2 < 0:
-                return 0
-            if dp[ind1][ind2] != -1:
-                return dp[ind1][ind2]
-            if text1[ind1] == text2[ind2]:
-                dp[ind1][ind2] = 1 + longsub(ind1-1, ind2-1)
-            else:
-                dp[ind1][ind2]= max(longsub(ind1-1,ind2),longsub(ind1,ind2-1))
-            return dp[ind1][ind2]
-        return longsub(m-1,n-1)
+        for i in range(m+1):
+            dp[i][0] = 0
+        for j in range(n+1):
+            dp[0][j] = 0
+        for ind1 in range(1,m+1):
+            for ind2 in range(1,n+1):
+                if text1[ind1-1] == text2[ind2-1]:
+                    dp[ind1][ind2] = 1 + dp[ind1-1][ind2-1]
+                else:
+                    dp[ind1][ind2]= max(dp[ind1-1][ind2],dp[ind1][ind2-1])
+        return dp[m][n]
+        
