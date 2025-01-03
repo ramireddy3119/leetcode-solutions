c69b35b45cdc74e5180086f8aa67d4a8e6400cd3
Ramireddy Mallikarjuna Reddy <rrmr3119@gmail.com>
2025-01-03 15:58:21 +0530

    Time: 51 ms (73.94%) | Memory: 32.3 MB (58.91%) - LeetSync

--- 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.py
+++ 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.py
@@ -3,11 +3,16 @@ class Solution:
         prefix = []
         curr = 0
         count = 0
-        for i in range(len(nums)):
-            curr += nums[i]
-            prefix.append(curr)
+        # for i in range(len(nums)):
+        #     curr += nums[i]
+        #     prefix.append(curr)
+        totalSum = 0
+        for i in nums:
+            totalSum += i
+        
         for i in range(len(nums)-1):
-            rem = prefix[-1] - prefix[i]
-            if prefix[i] >= rem:
+            curr += nums[i]
+            rem = totalSum - curr
+            if curr >= rem:
                 count += 1
         return count
