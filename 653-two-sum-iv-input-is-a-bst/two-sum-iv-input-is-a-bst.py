581f7f25df06019cdd737d0646f3c49b38c87b49
Ramireddy Mallikarjuna Reddy <rrmr3119@gmail.com>
2025-01-29 21:39:32 +0530

    Time: 15 ms (19.94%) | Memory: 19.9 MB (46.21%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.py
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.py
@@ -1,18 +1,43 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class BSTIterator:
+    def __init__(self, root, reverse=False):
+        self.stack = []
+        self.reverse = reverse
+        self._push_nodes(root)
+
+    def _push_nodes(self, node):
+        while node:
+            self.stack.append(node)
+            node = node.right if self.reverse else node.left  
+
+    def next(self):
+        if not self.stack:
+            return None
+        node = self.stack.pop()
+        self._push_nodes(node.left if self.reverse else node.right)
+        return node.val
+
+    def has_next(self):
+        return len(self.stack) > 0
+
+
 class Solution:
     def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
-        def isFound(root):
-            if not root:
-                return False
-            rem = k - root.val
-            if rem in seen:
+        if not root:
+            return False
+
+        left_iter = BSTIterator(root, reverse=False)  # Smallest values first
+        right_iter = BSTIterator(root, reverse=True)  # Largest values first
+
+        left_val = left_iter.next()
+        right_val = right_iter.next()
+
+        while left_val is not None and right_val is not None and left_val < right_val:
+            current_sum = left_val + right_val
+            if current_sum == k:
                 return True
-            seen.add(root.val)
-            return isFound(root.right) or isFound(root.left)
-        seen = set()
-        return isFound(root)
+            elif current_sum < k:
+                left_val = left_iter.next()  # Move left iterator forward
+            else:
+                right_val = right_iter.next()  # Move right iterator backward
+
+        return False
