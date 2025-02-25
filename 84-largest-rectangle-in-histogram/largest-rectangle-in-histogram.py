df7bf9d6f42091befd45113cbbdd6cf7bf99dc5c
Ramireddy Mallikarjuna Reddy <rrmr3119@gmail.com>
2025-02-25 10:34:54 +0530

    Time: 124 ms (97.43%) | Memory: 30.5 MB (57.21%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
@@ -1,28 +1,20 @@
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
+        n = len(heights)
+        stack = [] 
+        max_area = 0
+    
+        for i in range(n):
+            while stack and heights[stack[-1]] > heights[i]:
+                height = heights[stack.pop()]
+                width = i if not stack else (i - stack[-1] - 1)
+                max_area = max(max_area, height * width)
+            stack.append(i)
 
-        def next_smaller_elements(arr):
-            nse = [0] * len(arr)
-            stack = []
-            for i in range(len(arr) - 1, -1, -1):
-                while stack and arr[stack[-1]] >= arr[i]:
-                    stack.pop()
-                nse[i] = len(arr) if not stack else stack[-1]
-                stack.append(i)
-            return nse
+        while stack:
+            height = heights[stack.pop()]
+            width = n if not stack else (n - stack[-1] - 1)
+            max_area = max(max_area, height * width)
+    
+        return max_area
         
-        def prev_smaller_elements(arr):
-            pse = [0] * len(arr)
-            stack = []
-            for i in range(len(arr)):
-                while stack and arr[stack[-1]] > arr[i]:
-                    stack.pop()
-                pse[i] = -1 if not stack else stack[-1]
-                stack.append(i)
-            return pse
-        nse = next_smaller_elements(heights)
-        pse = prev_smaller_elements(heights)
-        maxi = 0
-        for i in range(len(heights)):
-            maxi = max(maxi,heights[i]*(nse[i]-pse[i]-1))
-        return maxi
