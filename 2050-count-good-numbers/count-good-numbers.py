77b1f5aa4e5e7a204b18f1fdb5149926c735f96d
Ramireddy Mallikarjuna Reddy <rrmr3119@gmail.com>
2025-04-13 12:18:42 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (99.93%) - LeetSync

--- 2050-count-good-numbers/count-good-numbers.py
+++ 2050-count-good-numbers/count-good-numbers.py
@@ -1,8 +1,13 @@
 class Solution:
     def countGoodNumbers(self, n: int) -> int:
-        a= (n+1)//2
-        b=n//2
-        mod = 10**9+7
-        return pow(5,a,mod) *pow(4,b,mod)%mod
-        
+        MOD = 1000000007
+        def binaryExp(a,b):
+            res = 1
+            while b:
+                if b & 1:
+                    res = (res * a) % MOD
+                a = (a * a) % MOD
+                b //= 2
+            return res
+        return (binaryExp(4,n//2)*binaryExp(5,n-n//2))%MOD
         
