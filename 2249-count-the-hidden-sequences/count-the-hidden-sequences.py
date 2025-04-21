d1f4d0d87363efd2aa63514e6a7bb9c0ac38425a
Ramireddy Mallikarjuna Reddy <rrmr3119@gmail.com>
2025-04-21 22:20:05 +0530

    Time: 30 ms (96.97%) | Memory: 32.4 MB (57.58%) - LeetSync

--- 2249-count-the-hidden-sequences/count-the-hidden-sequences.py
+++ 2249-count-the-hidden-sequences/count-the-hidden-sequences.py
@@ -5,8 +5,10 @@ class Solution:
         curr_val = 0
         for i in differences:
             curr_val += i
-            max_val = max(max_val,curr_val)
-            min_val = min(min_val,curr_val)
+            if curr_val > max_val:
+                max_val = curr_val
+            if curr_val < min_val:
+                min_val = curr_val
         count = (upper-lower)-(max_val - min_val) + 1
         return count if count > 0 else 0
         
