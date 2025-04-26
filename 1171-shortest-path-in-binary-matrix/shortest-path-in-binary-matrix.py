e0432aa53e6dcad13114502d61e7260c0ed04f75
Ramireddy Mallikarjuna Reddy <rrmr3119@gmail.com>
2025-04-26 22:53:29 +0530

    Time: 150 ms (78.09%) | Memory: 18.2 MB (91.77%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
@@ -1,26 +1,29 @@
+from collections import deque
+from typing import List
+
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        rows = len(grid)
-        cols = len(grid[0])
-        if grid[0][0] == 1:
+        rows, cols = len(grid), len(grid[0])
+        if grid[0][0] == 1 or grid[rows-1][cols-1] == 1:
             return -1
-        queue = deque([(0,0,1)])
-        visited = [[float('inf')]*cols for _ in range(rows)]
-        end = (rows-1,cols-1)
-        visited[0][0] = 1
+        
+        queue = deque([(0, 0, 1)])  # (row, col, distance)
+        visited = [[False] * cols for _ in range(rows)]
+        visited[0][0] = True
+
         directions = [
             (-1, 0), (1, 0), (0, -1), (0, 1),
-            (-1, -1), (-1, 1), (1, -1), (1, 1) 
+            (-1, -1), (-1, 1), (1, -1), (1, 1)
         ]
 
         while queue:
-            r,c,dist = queue.popleft()
-            if (r,c) == end:
-                return visited[r][c]
-            for dr,dc in directions:
-                nr,nc = r + dr ,c + dc
-                if 0 <= nr < rows and 0 <= nc < cols and  visited[nr][nc] == float('inf') and grid[nr][nc] != 1:
-                    visited[nr][nc] = dist + 1
-                    queue.append((nr,nc,dist+1))
-        return -1
+            r, c, dist = queue.popleft()
+            if (r, c) == (rows-1, cols-1):
+                return dist
+            for dr, dc in directions:
+                nr, nc = r + dr, c + dc
+                if 0 <= nr < rows and 0 <= nc < cols and not visited[nr][nc] and grid[nr][nc] == 0:
+                    visited[nr][nc] = True
+                    queue.append((nr, nc, dist + 1))
         
+        return -1
