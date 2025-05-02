c2b84728c93ef19b7fac2e0400e7ed408da9ddc5
Ramireddy Mallikarjuna Reddy <rrmr3119@gmail.com>
2025-05-02 19:50:22 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (69.86%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
@@ -1,24 +1,23 @@
-from collections import deque
-from typing import List
-
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
         graph = [[] for _ in range(n)]
-        for f, t, p in flights:
-            graph[f].append((t, p))
-        
-        q = deque([(0, (src, 0))])  # Correct syntax
-        dist = [float('inf')] * n
+        for f,t,p in flights:
+            graph[f].append((t,p))
+        q = deque([(0,(src,0))])
+        dist = [float('inf')]*n
         dist[src] = 0
-
         while q:
-            stop, (node, dis) = q.popleft()
+            it = q.popleft()
+            stop = it[0]
+            node = it[1][0]
+            dis = it[1][1]
             if stop > k:
                 continue
-            for neighbour, ndis in graph[node]:
-                newdis = dis + ndis
-                if newdis < dist[neighbour]:
+            for neighbour,ndis in graph[node]:
+                newdis = ndis + dis
+                if newdis < dist[neighbour] and stop <= k:
                     dist[neighbour] = newdis
-                    q.append((stop + 1, (neighbour, newdis)))
-        
+                    q.append((stop+1,(neighbour,newdis)))
         return dist[dst] if dist[dst] != float('inf') else -1
+
+
