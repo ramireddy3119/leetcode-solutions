4850983d5e25f10cd6c4023197b17a785fb1b651
Ramireddy Mallikarjuna Reddy <rrmr3119@gmail.com>
2025-06-03 16:32:06 +0530

    Time: 7 ms (79.47%) | Memory: 19.3 MB (89.42%) - LeetSync

--- 740-delete-and-earn/delete-and-earn.py
+++ 740-delete-and-earn/delete-and-earn.py
@@ -1,13 +1,14 @@
 class Solution:
     def deleteAndEarn(self, nums: List[int]) -> int:
-        arr = [0]*10001
+        l = max(nums)
+        arr = [0]*(l+1)
         for i in nums:
             arr[i] += i
         print(arr)
         maxi = 0
         a = 0
         b = 0
-        for i in range(10001):
+        for i in range(l+1):
             maxi = max(a,b+arr[i])
             b = a
             a = maxi
