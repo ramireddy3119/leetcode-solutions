0c29769032c065836014033cacc4ed11fb48d718
Ramireddy Mallikarjuna Reddy <rrmr3119@gmail.com>
2025-06-04 10:58:32 +0530

    Time: 2 ms (28.40%) | Memory: 18 MB (49.00%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.py
+++ 63-unique-paths-ii/unique-paths-ii.py
@@ -1,20 +1,20 @@
+from typing import List
+
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
-        rows = len(obstacleGrid)
-        col = len(obstacleGrid[0])
-        if obstacleGrid[0][0] == 1 or obstacleGrid[rows - 1][col - 1] == 1:
-            return 0
-        dp =[[0 for _ in range(col)] for _ in range(rows)]
-        dp[0][0] = 1
-        for i in range(rows):
-            for j in range(col):
-                if obstacleGrid[i][j] == 1:
-                    dp[i][j] = 0
-                elif i == 0 and j == 0:
-                    continue
-                else:
-                    up = dp[i-1][j]
-                    left = dp[i][j-1]
-                    dp[i][j] = up + left
-        return dp[rows-1][col-1]
+        def dfs(i, j, r, c, mat, memo):
+            if i < 0 or i >= r or j < 0 or j >= c or mat[i][j] == 1:
+                return 0
+            if (i, j) in memo:
+                return memo[(i, j)]
+            if i == r - 1 and j == c - 1:
+                return 1
+            memo[(i, j)] = dfs(i + 1, j, r, c, mat, memo) + dfs(i, j + 1, r, c, mat, memo)
+            return memo[(i, j)]
 
+        if obstacleGrid[0][0] == 1 or obstacleGrid[-1][-1] == 1:
+            return 0
+        m = len(obstacleGrid)
+        n = len(obstacleGrid[0])
+        memo = {}
+        return dfs(0, 0, m, n, obstacleGrid, memo)
