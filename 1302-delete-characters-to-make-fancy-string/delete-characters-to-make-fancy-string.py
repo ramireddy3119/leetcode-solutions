a40e71d4cfa874a4e411bd51716d86355cd9de76
Ramireddy Mallikarjuna Reddy <rrmr3119@gmail.com>
2025-07-21 13:04:41 +0530

    Time: 515 ms (14.12%) | Memory: 20.5 MB (8.95%) - LeetSync

--- 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.py
+++ 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.py
@@ -1,14 +1,15 @@
 class Solution:
     def makeFancyString(self, s: str) -> str:
+        n = len(s)
+        arr = [0]*len(s)
+        for i in range(1,n-1):
+            if s[i-1]  == s[i] and s[i] == s[i+1]:
+                arr[i] = 1
         res = ""
-        count = 1
-        for i in range(len(s)):
-            if i > 0:
-                if s[i-1] == s[i]:
-                    count += 1
-                else:
-                    count = 1
-            if count < 3:
+        for i in range(n):
+            if arr[i] == 0:
                 res += s[i]
-                
         return res
+
+
+        
