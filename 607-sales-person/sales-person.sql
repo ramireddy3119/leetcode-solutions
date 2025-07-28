c30d770bd951f98f0f7ea58d0ee4dc125eb5ed65
Ramireddy Mallikarjuna Reddy <rrmr3119@gmail.com>
2025-07-28 11:13:16 +0530

    Time: 1678 ms (15.53%) | Memory: 0B (100.00%) - LeetSync

---
+++ 607-sales-person/sales-person.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+select name 
+from SalesPerson
+where sales_id not in(
+    select sales_id
+    from Orders o
+    join Company c
+    on c.com_id = o.com_id
+    where c.name = 'RED'
+)
